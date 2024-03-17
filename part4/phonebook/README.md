36201e15b0c475dadeae6286ab5352afe35e831a
Guillermo <guillesafran@gmail.com>
2024-03-17 15:02:22 -0300

    hotfix

--- part4/phonebook/README.md
+++
@@ -1,5 +0,0 @@
-# phoneebook_backend
-
-Link to the deployed backend: https://phonebook-bmxi.onrender.com/
-
-The folder called request provide some useful endpoint connections to test the functionality of the backed
--- part4/phonebook_front/src/components/Filter.jsx
+++
@@ -1,8 +0,0 @@
-export const Filter = ({setFilterNames, filterNames}) => {
-  return (
-    <form onSubmit={e => e.preventDefault()}>
-      <label htmlFor="filter-name">filter shown with</label>
-      <input id="filter-name" onChange={e => setFilterNames(e.target.value)} value={filterNames} type="text" />
-    </form>
-  )
-}
